9468aebadc3b37b561db47c9ede8955d649ca0a1
unknown <you@example.com>
2022-11-27 18:58:20 +0400

    Task

---
+++ Extension/Extension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension
+{
+    public static class Extensions
+    {
+        public static bool CustomsContains(this string word, string value)
+        {
+            return word.IndexOf(value, StringComparison.CurrentCultureIgnoreCase) != -1;
+        }
+        public static string[] SplitCustom(this string @this, string separator, StringSplitOptions option)
+        {
+            return @this.Split(new[] { separator }, option);
+        }
+    }
+}
---
+++ Extension/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using Extension;
+
+
+#region CustomContains
+//string name = "Bu bir custom contains methodudur";
+//Console.WriteLine(name.CustomsContains("Bu"));
+//Console.WriteLine(name.CustomsContains("bu"));
+//Console.WriteLine(name.CustomsContains("tains"));
+//Console.WriteLine(name.CustomsContains("ns Methodudur"));
+//Console.WriteLine(name.CustomsContains("bu bir custom")); 
+#endregion
+
+
+#region CustomSplit
+//string metn = "Bu bir custom contains methodudur";
+
+
+//string[] words = metn.SplitCustom(" ", StringSplitOptions.RemoveEmptyEntries);
+
+//foreach (var word in words)
+//{
+//    Console.WriteLine(word);
+//}
+#endregion
